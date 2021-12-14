b7cef77859c03ab0e593597964e45abf05513baf
Anant Shukla <anantshukla1402@gmail.com>
2021-12-14 17:15:56 +0530

    response format changed

--- Hemo backend/index.js
+++ Hemo backend/index.js
@@ -22,10 +22,8 @@ app.use((req, res, next)=>{
 app.use((err, req, res, next) =>{
     res.status(err.status || 500);
     res.send({
-        error: {
             status: err.status || 500,
             message: err.message
-        }
     })
 })
 

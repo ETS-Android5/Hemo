3488b2b39a96093f13ef27ee334e025db451fe40
darpan1107 <mittal11darpan@gmail.com>
2021-12-18 17:47:44 +0530

    map box integration

--- Blood App/app/src/main/java/com/dev334/blood/ui/home/RequestFragment.java
+++ Blood App/app/src/main/java/com/dev334/blood/ui/home/RequestFragment.java
@@ -1,20 +1,18 @@
 package com.dev334.blood.ui.home;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
 import com.dev334.blood.R;
 import com.dev334.blood.databinding.FragmentRequestBinding;
 import com.dev334.blood.model.ApiResponse;
 import com.dev334.blood.model.Blood;
-import com.dev334.blood.model.User;
 import com.dev334.blood.util.retrofit.ApiClient;
 import com.dev334.blood.util.retrofit.ApiInterface;
 

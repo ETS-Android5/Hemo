523159e79921b277cc885f7d488621b069b0d33d
Aditya Kashyap <kashyapaditya1234@gmail.com>
2021-12-20 15:03:24 +0530

    ProfileFragment v.1.0.1

--- Blood App/app/src/main/java/com/dev334/blood/ui/home/ProfileFragment.java
+++ Blood App/app/src/main/java/com/dev334/blood/ui/home/ProfileFragment.java
@@ -6,21 +6,30 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dev334.blood.R;
 import com.dev334.blood.databinding.FragmentProfileBinding;
+import com.dev334.blood.ui.login.LoginActivity;
+import com.dev334.blood.ui.login.LoginFragment;
+import com.dev334.blood.util.app.AppConfig;
 
 public class ProfileFragment extends Fragment {
 
     public static ProfileFragment fragment=null;
     private View view;
     FragmentProfileBinding binding;
+    private String TAG="ProfileFragment";
+    private AppConfig appConfig;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -44,10 +53,12 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false);
 
+        appConfig = new AppConfig(getContext());
         binding.settingsChangePass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i = new Intent(getActivity(), ChangePassword.class);
+                startActivity(i);
             }
         });
 
@@ -98,14 +109,40 @@ public class ProfileFragment extends Fragment {
         binding.settingsLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                appConfig.setAuthToken("");
+                appConfig.setLoginStatus(false);
+                appConfig.setProfileCreated(false);
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
             }
         });
 
         binding.settingsDelete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                AlertDialog.Builder alert=new AlertDialog.Builder(getContext());
+                View view=getLayoutInflater().inflate(R.layout.dialog_delete_account,null);
+                TextView delete=view.findViewById(R.id.dDeleteAcc);
+                TextView cancel=view.findViewById(R.id.dCancel);
+                alert.setView(view);
+                AlertDialog show=alert.show();
+                delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
+                    }
+                });
+
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        show.dismiss();
+                    }
+                });
+
+                alert.setCancelable(true);
+                show.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             }
         });
         return binding.getRoot();

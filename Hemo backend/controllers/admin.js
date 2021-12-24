c69b916e9e4a6a02c3677bd3c8fa26ff122ea063
Anant Shukla <anantshukla1402@gmail.com>
2021-12-24 17:39:17 +0530

    request admin backend

--- Hemo backend/controllers/admin.js
+++ Hemo backend/controllers/admin.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const createError = require('http-errors');
 const Schedule = require('../model/schedule');
+const Blood = require('../model/Blood');
 
 exports.get_all_schedule = async (req, res, next)=>{
     try{
@@ -19,7 +20,10 @@ exports.approve_schedule = async (req, res, next)=>{
         const id = req.query.id
         const approval = !!parseInt(req.query.approval)
         await Schedule.updateOne({_id: id},{approval, pending: false})
-        res.status(200).send("approval updated")
+        res.status(200).send({
+            status: 200,
+            message: "approval updated"
+        })
     }catch(error){
         next(error)
         return
@@ -31,7 +35,41 @@ exports.status_schedule = async (req, res, next)=>{
         const id = req.query.id
         const status = !!parseInt(req.query.status)
         await Schedule.updateOne({_id: id},{status, close:true})
-        res.status(200).send("status updated")
+        res.status(200).send({
+            status: 200,
+            message: "status updated"
+        })
+    }catch(error){
+        next(error)
+        return
+    }
+}
+
+exports.get_all_requests = async(req, res, next)=>{
+    try{
+        const location =  req.query.location
+        const bloods = await Blood.find({location, verified: false})
+        res.status(200).send(bloods);
+
+    }catch(error){
+        next(error)
+        return;
+    }
+}
+
+exports.verify_request = async(req, res, next)=>{
+    try{
+        const _id = req.query.id;
+        const verified = !!parseInt(req.query.id)
+        if(!verified){
+            await Blood.updateOne({_id}, {verified, status: false})
+        }else{
+            await Blood.updateOne({_id}, {verified})
+        }
+        res.status(200).send({
+            status: 200,
+            message: "verify updated"
+        })
     }catch(error){
         next(error)
         return

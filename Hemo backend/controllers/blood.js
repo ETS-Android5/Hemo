50a78c0ea7fe240d970ba7916cb89d6ecda97604
Anant Shukla <anantshukla1402@gmail.com>
2021-12-25 02:02:08 +0530

    schedule error

--- Hemo backend/controllers/blood.js
+++ Hemo backend/controllers/blood.js
@@ -61,11 +61,16 @@ exports.blood_schedule = async (req, res, next)=>{
     }
 
     try{
+
+        const{name, user, bank, date, time} = req.body;
+
         const token = "cJ4l2E5jSOiiQDPxtnx912:APA91bE6CmXSynk5zWrRx_abzTy-bOAstiEL9S59c4Vj4p23ScNj5lm0lJn31RX8TKQCTdHAqRrjXQi-wYtO6wPFfOQLYH9FfcTPGupTR3eaRhgtLM4zKCdc5K9VMNxQlebeY3CzarHI";
         const schedule = new Schedule(req.body)
         await schedule.save();
 
-        notif.send_notificaiton(token, "Appointment Scheduled", "Your blood can save someone's life")
+        //notif.send_notificaiton(token, "Appointment Scheduled", "Your blood can save someone's life")
+
+        sendVerificationEmail(name, user, "anantshukla1402@gmail.com", date, time, bank)
 
         res.status(200).send({
             status: 200,
@@ -77,3 +82,29 @@ exports.blood_schedule = async (req, res, next)=>{
     }
 
 }
+
+const sendVerificationEmail = ({ name, _id, email, date, time, bank}, res, next) => {
+    //url to be used in the email
+  
+    const uniqueString = uuidv4() + _id;
+    const curUrl = "http://localhost:3000/" + "api/user/verify/" + uniqueString;
+  
+    var mailOptions = {
+      from: process.env.AUTH_EMAIL,
+      to: email,
+      subject: "Email verification Hemo Dev334",
+      html: pug.renderFile('F:\Software Projects\Hemo\Hemo backend\views\schedule.pug', { name: name, user_id: _id, date: date, time: time, bank: bank})
+    };
+    transporter.sendMail(mailOptions, async function (error, response) {
+      if (error) {
+        next(error);
+        return;
+      } else {
+        res.status = 200;
+        res.send({
+          status: 200,
+          message: "Email sent",
+        });
+      }
+    });
+  };
